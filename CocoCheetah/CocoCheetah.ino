8b55242e742d98e20f9ebd1909b46be99381d3f9
Cocochief <cocochief4@gmail.com>
2021-09-17 14:07:38 -0700

    Add Turning
    Turn Test 1

--- CocoCheetah/CocoCheetah.ino
+++ CocoCheetah/CocoCheetah.ino
@@ -2,6 +2,7 @@
 
 #define LRpin 12
 #define FBpin 13
+#define turnPin 45
  
 // the actual values for "fast" and "slow" depend on the motor
 #define PWM_SLOW 100  // arbitrary slow speed PWM duty cycle
@@ -13,19 +14,21 @@
 struct PolarCoord {
   float r;
   float theta;
+  float turn;
 };
 struct motor {
   int pinDir;
   int pinPwm;
   float thetaShift;
   float force;
+  float turnMultiplier;
 };
 
 motor motorArr[4] = {
-  { 10, 11, 45, 0 }, //Motor B (FR)
-  { 8, 9, 135, 0 }, //Motor A (FL)
-  { 3, 4, 45, 0 }, //Motor C (BL)
-  { 5, 6, 135, 0 } //Motor D (BR)
+  { 10, 11, 45, 0, -1 }, //Motor B (FR)
+  { 8, 9, 135, 0, 1 }, //Motor A (FL)
+  { 3, 4, 45, 0, 1 }, //Motor C (BL)
+  { 5, 6, 135, 0, -1 } //Motor D (BR)
   };
 
 int LRval;
@@ -43,6 +46,7 @@ void setup() {
   
   pinMode(LRpin, INPUT);
   pinMode(FBpin, INPUT);
+  pinMode(turnPin, INPUT);
   Serial.begin(9600);
   Serial.println("Serial On");
 }
@@ -50,7 +54,8 @@ void setup() {
 void loop() {
   LRval = pulseIn(LRpin, HIGH);
   FBval = pulseIn(FBpin, HIGH);
-  polar = EuclidPolar(LRval, FBval);
+  turnVal = pulseIn(turnPin, HIGH);
+  polar = EuclidPolar(LRval, FBval, turnVal);
   MapCoord = String("(") + LRval + "," + FBval + ")";
   Serial.println(String("Polar Coords: ") + polar.r + ", " + polar.theta);
   Serial.println(MapCoord);
@@ -59,9 +64,10 @@ void loop() {
   motorDrive();
 }
 
-PolarCoord EuclidPolar(int x, int y) {
+PolarCoord EuclidPolar(int x, int y, int z) {
   float LRCoord = x - 1500;
   float FBCoord = y - 1500;
+  float turnCoord = z - 1500;
   float r = sqrt(sq(LRCoord) + sq(FBCoord));
   r = r/500;
   float theta = -1 * atan2(LRCoord, FBCoord);
@@ -72,7 +78,8 @@ PolarCoord EuclidPolar(int x, int y) {
   if (r > 1 || r > 0.95) {
     r = 1;
   }
-  PolarCoord coord = { r, theta };
+  float turnPower = turnCoord/500;
+  PolarCoord coord = { r, theta, turnPower };
   return coord;
 }
 
@@ -84,15 +91,29 @@ void calcSine() {
 
 void scale() {
   float maxForce = 0;
+  float maxTurnForce = 0;
   for (int i = 0; i < motorArrCount; i++) {
     if (maxForce < abs(motorArr[i].force)) {
       maxForce = abs(motorArr[i].force);
     }
   }
-  //add turning here
+  //for (int i = 0; i < motorArrCount; i++) {
+    //motorArr[i].force = motorArr[i].force + polar.turnPower * motorArr[i].turnMultiplier;
+  }
   if (maxForce > 0) {
     for (int i = 0; i < motorArrCount; i++) {
       motorArr[i].force = motorArr[i].force * polar.r/maxForce;
+    }
+  }
+  for (int i = 0; i < motorArrCount; i++) {
+    motorArr[i].force = motorArr[i].force + polar.turnPower * motorArr[i].turnMultiplier;
+    if (maxTurnForce < abs(motorArr[i].force)) {
+      maxTurnForce = abs(motorArr[i].force);
+    }
+  }
+  if (maxTurnForce > 1) {
+    for (int i = 0; i < motorArrCount; i++) {
+      motorArr[i].force = motorArr[i].force * 1/maxTurnForce;
     }
   }
 }
